22d97af9ab47e898afb182e0e232e3c999e13598
stinja <projectmaydev@gmail.com>
2021-05-28 17:17:33 -0500

    more comments for learning

--- app.js
+++ app.js
@@ -10,3 +10,15 @@ app.get('/', (req, res) => { // Creates an app, takes in the values of req and r
 app.listen(port, () => { // Function to listen on port assigned above in const port = 3000
   console.log(`Example app listening at http://localhost:${port}`) // Confirms that app.listen is listening on the assigned port by printing out a statement in the console
 })
+
+
+/**
+ * Additional Notes
+ * 
+ * a JSON object
+ * { key: value }
+ * 
+ * ? is a delimeter which marks the beginning of a query string
+ * 
+ * ?foo=bar is a query string which creates an object with foo as the key and bar as the value
+ */
